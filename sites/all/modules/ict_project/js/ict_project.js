13c432d20a873353119d6e223bfa4a1b81291604
Alex Sadleir <alex.sadleir@linkdigital.com.au>
2014-12-18 08:22:00 +1100

    Fix total JS for field collection FY table

--- sites/all/modules/ict_project/js/ict_project.js
+++ sites/all/modules/ict_project/js/ict_project.js
@@ -11,11 +11,12 @@
     $('tr.sum input.form-text').keyup(function(){
       var table = $(this).parents('table').attr('id');
       $("#" + table +' td.total').each(function(i){
-        var sum = parseFloat($("#" +table+" td.opex[data-sum='" + i + "']").find('.form-text').val())+parseFloat($("#" +table+ " td.capex[data-sum='" + i + "']").find('.form-text').val());
+        var sum = 0;
+        sum += parseFloat($("#" +table+" td.opex[data-sum='" + i + "']").find('.form-text').val()) || 0;
+        sum += parseFloat($("#" +table+ " td.capex[data-sum='" + i + "']").find('.form-text').val()) || 0;
         var total =  (sum === parseFloat(sum, 10)) ? sum : '';
-        if (total) {
+
           $(this).find('.form-text').val(Number(total).toFixed(2));
-        }
       });
 
     });
