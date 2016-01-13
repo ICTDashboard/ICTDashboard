352c20c0701c546705d724a9fed7b87eac8a15ac
Andrey Deryabkin <aderyabkin@adyax.com>
2016-01-13 17:20:37 +0200

    TPDFINXCDDB-19 / minor fix

--- sites/all/themes/itdash/layouts/itdash_dashboard/itdash-itdash-dashboard.tpl.php
+++ sites/all/themes/itdash/layouts/itdash_dashboard/itdash-itdash-dashboard.tpl.php
@@ -1,10 +1,4 @@
 
-    <div class="page-title dotbg">
-      <div class="inner-title-content wrap cf">
-        <h1><?php print t('Project Dashboard'); ?></h1>
-      </div>
-    </div>
-
     <div class="wrap cf">
       <?php if ($content['content']) : ?>
         <?php print $content['content']; ?>
