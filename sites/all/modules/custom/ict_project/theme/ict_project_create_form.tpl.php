ee23010c425e0eee840564f79e8a6262e5d2210d
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2016-01-15 16:13:27 +0200

    TPDFINXCDDB-25 / create project

---
+++ sites/all/modules/custom/ict_project/theme/ict_project_create_form.tpl.php
@@ -0,0 +1,104 @@
+<div class="page-title dotbg">
+	<div class="inner-title-content wrap cf">
+		<h1>Create project</h1>
+	</div>
+</div>
+<div id="inner-content" class="wrap cf">
+	<h2>Baseline data</h2>
+	<div class="project-update-submission d-all">
+		<form class="node-form node-project-form" action="/node/add/project" method="post" id="project-node-form" accept-charset="UTF-8">
+			<table>
+        <tr>
+          <td class="label">
+            <?php
+            print $form['field_portfolio_name']['#title'];
+            ?>
+          </td>
+          <td class="text">
+            <?php
+            print render($form['field_portfolio_name']);
+            ?>
+          </td>
+        </tr>
+
+				<tr>
+					<td class="label">
+						<?php
+							print $form['field_government_entity_name']['#title'];
+						?>
+					</td>
+					<td class="text">
+						<?php
+							print render($form['field_government_entity_name']);
+						?>
+					</td>
+				</tr>
+
+				<tr>
+					<td class="label">
+						<?php
+							print $form['title']['#title'];
+						?>
+					</td>
+					<td class="text">
+						<?php
+							print render($form['title']);
+						?>
+					</td>
+				</tr>
+
+				<tr><td colspan="2"><div class="dotted-line"></div></td></tr>
+
+				<tr><td colspan="2"><h2>User information</h2></td></tr>
+
+        <tr>
+          <td class="label">
+            <?php
+            print $form['field_department']['#title'];
+            ?>
+          </td>
+          <td class="text">
+            <?php
+            print render($form['field_department']);
+            ?>
+          </td>
+        </tr>
+
+				<tr>
+					<td class="label">
+						<?php
+						print t('Data Approvers');
+						?>
+					</td>
+					<td class="text">
+						<?php
+						print render($form['approvers']);
+						?>
+					</td>
+				</tr>
+
+				<tr>
+					<td class="label">
+						<?php
+						print t('Data Editors');
+						?>
+					</td>
+					<td class="text">
+						<?php
+						print render($form['editors']);
+						?>
+					</td>
+				</tr>
+
+			</table>
+      <div style="display:none;">
+        <?php
+        print drupal_render_children($form);
+        ?>
+      </div>
+			<p class="submit">
+        <?php print render($form['submit']) ;?>
+      </p>
+		</form>
+	</div>
+</div>
