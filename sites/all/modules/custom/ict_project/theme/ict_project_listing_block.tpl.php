1cca306186d31bd0b0a388c749b1980d6f2ad0cf
Andrey Deryabkin <aderyabkin@adyax.com>
2016-01-18 17:45:34 +0200

    Workflow accsse helpers

--- sites/all/modules/custom/ict_project/theme/ict_project_listing_block.tpl.php
+++ sites/all/modules/custom/ict_project/theme/ict_project_listing_block.tpl.php
@@ -91,7 +91,7 @@
                   <a href="<?php print url('node/' . $project->nid); ?>">
                     <span><?php print t('View'); ?></span>
                   </a>
-                  <?php if ($user->uid == 1 || in_array($user->uid, ict_project_get_users($project->nid, ICT_PROJECT_EDITOR_ACCESS))) : ?>
+                  <?php if (ict_project_access_update('create', $project->nid)) : ?>
                     <a href="<?php print url('project/' . $project->nid . '/update'); ?>">
                       <span><?php print t('Create an update draft'); ?></span>
                     </a>
