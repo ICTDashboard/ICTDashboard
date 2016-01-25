a7db69a5c74cf5c07a9f49ae506c29c544d478e0
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2016-01-25 18:36:53 +0200

    TPDFINXCDDB-29 / budget table

--- sites/all/modules/custom/ict_project/theme/baseline/field_original_total_budget_table.tpl.php
+++ sites/all/modules/custom/ict_project/theme/baseline/field_original_total_budget_table.tpl.php
@@ -5,19 +5,22 @@
   <tr>
     <th><?php print t('FY (yy/yy)'); ?></th>
     <?php  foreach ($element_keys as $item) : ?>
-      <?php if ($item === 'add_more') continue; ?>
+      <?php if (!is_numeric($item)) continue; ?>
       <th>
         <div style="display: none;">
           <?php print render($element[$item]['field_year']); ?>
         </div>
-        <?php print $element[$item]['field_year'][LANGUAGE_NONE][0]['value']['#value']; ?>
+        <?php print $element[$item]['field_year'][LANGUAGE_NONE][0]['value']['#default_value']; ?>
+        <?php if ($item == $element['#max_delta']) : ?>
+          <?php print render($element[$item]['remove_button']); ?>
+        <?php endif; ?>
       </th>
     <?php endforeach; ?>
   </tr>
   <tr class="sum">
     <td><div class="label"><?php print t('Baseline'); ?></div></td>
     <?php foreach ($element_keys as $item) : ?>
-      <?php if ($item === 'add_more') continue; ?>
+      <?php if (!is_numeric($item)) continue; ?>
       <td>
         <?php print render($element[$item]['field_total']); ?>
       </td>
@@ -25,4 +28,7 @@
   </tr>
   </tbody>
 </table>
+<?php if (isset($element['add_more_before'])) : ?>
+  <?php print render($element['add_more_before']); ?>&nbsp;
+<?php endif; ?>
 <?php print render($element['add_more']); ?>
