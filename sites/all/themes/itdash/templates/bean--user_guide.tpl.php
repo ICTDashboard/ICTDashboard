6ea8d5e581f248869b8f19ff407181209dfda7c2
Motornyuk Sergey <tonnydexter@gmail.com>
2016-01-26 16:13:07 +0200

    TPDFINXCDDB-20 / user guide

---
+++ sites/all/themes/itdash/templates/bean--user_guide.tpl.php
@@ -0,0 +1,11 @@
+<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      print render($content);
+      if (bean_access('update',$variables['elements']['#bundle'])){
+            print l('<span>'.t('Edit').'</span>', $url . '/edit', ['attributes' => ['class' => 'general-button'], 'html' => TRUE] );
+      }
+    ?>
+  </div>
+</div>
