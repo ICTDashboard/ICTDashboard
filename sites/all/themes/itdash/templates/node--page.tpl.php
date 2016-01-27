7a000e3d9a3d6b8c48eb21ce8d051eb17ad1c582
Motornyuk Sergey <tonnydexter@gmail.com>
2016-01-27 16:58:18 +0200

    TPDFINXCDDB-22 / rewrited template of basic page

---
+++ sites/all/themes/itdash/templates/node--page.tpl.php
@@ -0,0 +1,9 @@
+<div class="page-title dotbg">
+  <div class="inner-title-content wrap cf">
+    <h1><?php print $title; ?></h1>
+  </div>
+</div>
+
+<div id="inner-content" class="wrap cf">
+    <?php print render($content); ?>
+</div>
