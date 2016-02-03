6b74484500984ae8debc26d0cca1ea6315fbcdfe
Andrey Deryabkin <aderyabkin@adyax.com>
2016-02-03 17:06:56 +0200

    validation fix

--- sites/all/themes/itdash/templates/node--project.tpl.php
+++ sites/all/themes/itdash/templates/node--project.tpl.php
@@ -268,7 +268,7 @@
     <?php if (!empty($update_node)) : ?>
       <?php print drupal_render(node_view($update_node)); ?>
     <?php endif; ?>
-    
+
     <?php if (!empty($diff)) : ?>
       <p><em><?php print t('* - fields that was updated.'); ?></em></p>
     <?php endif; ?>
