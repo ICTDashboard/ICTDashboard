5d17dbe3b1fa66dd883f97c84d893f36702aeabf
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2016-03-09 12:41:58 +0200

    TPDFINXCDDB-148 / initial commit

---
+++ sites/all/modules/custom/ict_project/theme/dashboard_overview/ict_projects_dashboard_overview_number_indicators.tpl.php
@@ -0,0 +1,21 @@
+<div id="ict-dashboard-overview-number-indicators">
+  <div class="wrap cf">
+    <div class="t-2of3 d-2of3">
+      it works!!
+    </div>
+    <div class="t-1of3 d-1of3" id="ict-dashboard-numbers">
+      <div class="label">
+        <?php print t('Number of Active Projects'); ?>
+        <a href="javascript:void(0);" class="tooltip">
+          <i class="tooltip-icon"></i>
+          <span class="tooltip-content">
+            <?php print t('The Portfolio for which the department or entity belongs.'); ?>
+          </span>
+        </a>
+      </div>
+      <div class="big-number">
+        <?php print '34' ?>
+      </div>
+    </div>
+  </div>
+</div>
