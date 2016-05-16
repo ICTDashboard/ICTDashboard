d36b809f47814cf5e74b39812cbb2540cbfdc33f
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2016-05-16 16:23:33 +0300

    TPDFINXCDDB-239 / viz fixes with data

--- sites/all/modules/custom/ict_project/theme/baseline/field_original_total_budget_graph_view.tpl.php
+++ sites/all/modules/custom/ict_project/theme/baseline/field_original_total_budget_graph_view.tpl.php
@@ -1,6 +1,21 @@
 <div style="margin-left: 25px; max-width: 945px;">
   <canvas id="budgetChart" width="945" height="325"></canvas>
-  <div class="legend" id="expenditure_legend"></div>
+  <div class="legend" id="expenditure_legend">
+    <ul class="bar-legend">
+      <li>
+        <span style="background-color:#ff6161"></span>
+        <?php print t('Total Expenditure to Date ($m)'); ?>
+      </li>
+      <li>
+        <span style="background-color:#5c46a4"></span>
+        <?php print t('Total Budget ($m)'); ?>
+      </li>
+      <li>
+        <span class="required">*</span>
+        <?php print t('Current Financial Year'); ?>
+      </li>
+    </ul>
+  </div>
 </div>
 <script>
   (function ($) {
@@ -11,10 +26,6 @@
         Drupal.settings.budget_chart.data,
         Drupal.settings.budget_chart.options
       );
-
-      document
-        .getElementById("expenditure_legend")
-        .innerHTML = ExpenditureChart.generateLegend();
 
     });
   })(jQuery);
