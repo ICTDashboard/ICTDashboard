ca9aa0c84ef6bab5327c993550cd53b8eee11c07
Artem Bazykin <artem.bazykin@linkdigital.com.au>
2016-05-30 13:00:34 +0300

    TPDFINXCDDB-263 / Fixed responsive design

--- sites/all/modules/custom/ict_viz/theme/ict-viz-home-projects.tpl.php
+++ sites/all/modules/custom/ict_viz/theme/ict-viz-home-projects.tpl.php
@@ -8,39 +8,44 @@
     t('Last'),
   );
 ?>
-<div class="wrap cf"><div class="ict-home-projects-table" id="ict-home-projects-table-id">
-  <h2><?php print $table_header; ?></h2>
-  <div class="table-config-bar">
-    <?php print render($pager); ?>
-  </div>
+<div class="wrap cf">
+  <div class="ict-home-projects-table" id="ict-home-projects-table-id">
+    <h2><?php print $table_header; ?></h2>
+    <div class="table-config-bar pager-tags-top">
+      <?php print render($pager); ?>
+      <?php print theme('pager', array('tags' => $pager_tags)); ?>
+    </div>
       <table>
         <tr class="<?php print !empty($query['direction']) ? 'sort-direction-' . $query['direction'] : ''; ?>">
           <th class="<?php print $col_classes[0] ?>">
             <?php print t('Entity'); ?>
-            <?php $query['order'] = 'eg_name'; ?>
-            <a class="ict-sort-link" href="<?php print url(current_path(), array('query' => $query, 'fragment' => 'ict-home-projects-table-id')); ?>"><span></span></a>
           </th>
           <th class="<?php print $col_classes[1] ?>">
             <?php print t('Project Title'); ?>
-            <?php $query['order'] = 'title'; ?>
-            <a class="ict-sort-link" href="<?php print url(current_path(), array('query' => $query, 'fragment' => 'ict-home-projects-table-id')); ?>"><span></span></a>
           </th>
           <th class="<?php print $col_classes[4] ?>">
             <?php print t('Project Schedule Status'); ?>
-            <?php $query['order'] = 'status'; ?>
-            <a class="ict-sort-link" href="<?php print url(current_path(), array('query' => $query, 'fragment' => 'ict-home-projects-table-id')); ?>"><span></span></a>
           </th>
         </tr>
         <tr class="tooltips-row">
           <th class="<?php print $col_classes[0] ?>"><a href="javascript:void(0);" class="tooltip"><i class="tooltip-icon"></i><span class="tooltip-content">
             <?php print t('The Government entity responsible for the project.'); ?>
-          </span></a></th>
+            </span></a>
+            <?php $query['order'] = 'eg_name'; ?>
+            <a class="ict-sort-link" href="<?php print url(current_path(), array('query' => $query, 'fragment' => 'ict-home-projects-table-id')); ?>"><span></span></a>
+          </th>
           <th class="<?php print $col_classes[1] ?>"><a href="javascript:void(0);" class="tooltip"><i class="tooltip-icon"></i><span class="tooltip-content">
             <?php print t('The official full name of the project'); ?>
-      </span></a></th>
+            </span></a>
+            <?php $query['order'] = 'title'; ?>
+            <a class="ict-sort-link" href="<?php print url(current_path(), array('query' => $query, 'fragment' => 'ict-home-projects-table-id')); ?>"><span></span></a>
+          </th>
           <th class="<?php print $col_classes[4] ?>"><a href="javascript:void(0);" class="tooltip"><i class="tooltip-icon"></i><span class="tooltip-content">
             <?php print t('A status of the project compared to its planned schedule'); ?>
-      </span></a></th>
+            </span></a>
+            <?php $query['order'] = 'status'; ?>
+            <a class="ict-sort-link" href="<?php print url(current_path(), array('query' => $query, 'fragment' => 'ict-home-projects-table-id')); ?>"><span></span></a>
+          </th>
         </tr>
         <?php foreach($results as $row) : ?>
           <tr>
