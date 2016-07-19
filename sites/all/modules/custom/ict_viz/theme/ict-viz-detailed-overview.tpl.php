8a6c8b0f25dde3d7182dcd6fa86778b1ad67915e
Yan <yan.rudneko@linkdigital.com.au>
2016-07-19 16:59:01 +0300

    TPDFINXCDDB-294 / Working with graph

--- sites/all/modules/custom/ict_viz/theme/ict-viz-detailed-overview.tpl.php
+++ sites/all/modules/custom/ict_viz/theme/ict-viz-detailed-overview.tpl.php
@@ -1,32 +1,7 @@
     <script src="http://d3js.org/d3.v3.min.js"></script>
     <script src="http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js"></script>
 <style type="text/css">
-    #detailed-view-expenditure {
-      overflow-x: scroll;
-    }
-    .axis path,
-      .axis line {
-      fill: none;
-      stroke: #000;
-      shape-rendering: crispEdges;
-    }
-    .axis line {
-      /*stroke: #F5F8FC;*/
-    }
-    .grid .tick {
-    stroke: lightgrey;
-    stroke-opacity: 0.7;
-    shape-rendering: crispEdges;
-    }
-    .bar1 {
-      fill: #FF6161;
-    }
-    .bar2 {
-      fill:#5C46A4;
-    }
-    .bar2:nth-of-type(6) {
-      fill: black;
-    }
+
 </style>
 <div id="ict-dashboard-detailed-overview">
   <div class="wrap cf">
@@ -58,10 +33,10 @@
               </span>
               <?php print t('Total Budget ($m)'); ?>
             </li>
-            <li>
+<!--             <li>
               <span class="required">*</span>
               <?php print t('Current Financial Year'); ?>
-            </li>
+            </li> -->
           </ul>
         </div>
     </div>
@@ -131,99 +106,94 @@
 </div>
 <script> 
 
-  var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 1250 - margin.left - margin.right,
-    height = 360 - margin.top - margin.bottom;
+jQuery(document).ready(function () {
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = 960 - margin.left - margin.right,
+      height = 360 - margin.top - margin.bottom;
 
     var data1 = Drupal.settings.detailed_budget_chart;
+    data1.data.datasets[0].data.shift();
+    data1.data.datasets[1].data.shift();
+    data1.data.labels.shift();
+    data1.data.datasets[0].data.pop();
+    data1.data.datasets[1].data.pop();
+    data1.data.labels.pop();
     var arr = [];
-    var space = [100, 200, 300, 400, 500, 600, 700,800, 900];
-    for(i = 0; i < 9; i++) {
-      arr.push({
-        x: data1.data.datasets[0].data[i],y: data1.data.datasets[1].data[i], years: data1.data['labels'][i], space : space[i]
-      });
-      // console.log(arr);
-    };
-    // console.log(arr);
+    var element = [];
 
-    var data2 = [
-    {x : 44.43, y: 55.88, z: '2011-12'},
-    {x : 168.7, y: 188.4, z: '2012-13'},
-    {x : 230.8, y: 259.6, z: '2013-14'},
-    {x : 234.69, y: 243.79, z: '2014-15'},
-    {x : 404.02, y: 639.4, z: '2015-16'},
-    {x : 10, y: 327.9, z: '2016-17'},
-    {x : 0, y: 189.6, z: '2017-18'},
-    {x : 0, y: 113.6, z: '2018-19'},
-    {x : 0, y: 100, z: '2019-20'},
-    ];
-    // console.log(data2);
+    for(i = 0; i < data1.data.labels.length; i++) {
+      arr.push({
+        x: data1.data.datasets[0].data[i],y: data1.data.datasets[1].data[i], years: data1.data['labels'][i], element : i
+      });
+      element.push(i);
+    };
+
     var data = [{ "x" : data1.data.datasets[0].data,
                  "y" : data1.data.datasets[1].data,
-                 "year" : data1.data['labels']}];
-    var year = data1.data['labels'];
+                 "year" : data1.data['labels'],
+                "element": element}];
 
     var x = d3.scale.ordinal()
-    .domain(data[0].year)
-    .rangeBands([0, width - margin.right]);
+      .domain(data[0].year)
+      .rangeBands([8, width - margin.right - 140]);
     
     var y = d3.scale.linear().range([height, 0]).domain([0,
-      d3.max(data[0].y, function (d) {
-        return d;
-      })
-    ]);
-
-    // var y2 = d3.scale.linear().range([height, 0]).domain([0,
-    //   d3.max(data[0].y, function (d) {
-    //     return d;
-    //   })
-    // ]);
+        d3.max(data[0].y, function (d) {
+          return d;
+        })
+      ]);
 
     var g = d3.scale.ordinal()
-    .domain(data[0].y)
-    .rangeBands([0, width - margin.right]);
+      .domain(data[0].element)
+      .rangeBands([0, width - margin.right - 160]);
 
-    // console.log(data.x);
     var g2 = d3.scale.ordinal()
-    .domain(data[0].y)
-    .rangeBands([0, width - margin.right]);
+      .domain(data[0].element)
+      .rangeBands([0, width - margin.right - 160]);
     
     var xAxis = d3.svg.axis()
-    .scale(x)
-    // .outerTickSize(0)
-    .orient("bottom");
+      .scale(x) 
+      .outerTickSize(0)
+      .orient("bottom");
 
     var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(15)
-        .tickFormat(function (d) {
+      .scale(y)
+      .orient("left")
+      .tickSize(0)
+      .tickFormat(function (d) {
         var prefix = d3.formatPrefix(d);
         return '$' + prefix.scale(d);
-    });
+      });
 
     var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([0, -70])
-    .html(function(d) {
-      return "<span style='border-radius: 5px;padding: 10px;background: #fff; display: block;color:black; font-weight:bold; font-size: 12px;'>"+ d.years + "</br><span style='display: inline-block;width: 10px;height: 10px;background: #FF6161;content:'';'></span> $" + d.x + "m" + "</br><span style='display: inline-block;width: 10px;height: 10px;background: #5C46A4;content:'';'></span> $" + d.y + "m" + "</span>";
-    })
+      .attr('class', 'd3-tip')
+      .offset([0, -70])
+      .html(function(d) {
+        return "<span style='border-radius: 5px;padding: 10px;background: #fff; display: block;color:black; font-weight:bold; font-size: 12px;'>"+ d.years + "</br><span style='display: inline-block;width: 10px;height: 10px;background: #FF6161;content:'';'></span> $" + d.x + "m" + "</br><span style='display: inline-block;width: 10px;height: 10px;background: #5C46A4;content:'';'></span> $" + d.y + "m" + "</span>";
+      })
 
 
   function make_y_axis() {
     return d3.svg.axis()
         .scale(y)
         .orient("left")
-        .ticks(15)
   }
 
-var chart = d3.select("#detailed-overview-chart")
+  var chart = d3.select("#detailed-overview-chart")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
  
   chart.call(tip);
+
+  chart.append("g")            
+    .attr("class", "grid")
+    .call(make_y_axis()
+      .tickSize(-width , 0, 0)
+      .tickFormat("")
+    )
 
   chart.append("g")
       .attr("class", "x axis")
@@ -234,21 +204,14 @@ var chart = d3.select("#detailed-overview-chart")
       .attr("class", "y axis")
       .call(yAxis)
 
-    chart.append("g")            
-        .attr("class", "grid")
-        .call(make_y_axis()
-          .tickSize(-width + margin.right, 0, 0)
-          .tickFormat("")
-        )
-
-
   chart.selectAll("div")
       .data(arr)
-    .enter().append("rect")
+      .enter().append("rect")
       .attr("class", "bar1")
-      .attr("x", function(d) {return g(d.y) + 37; })
+      .style("fill", "#FF6161")
+      .attr("x", function(d) {return g(d.element) + 20; })
       .attr("y",height)
-      .attr("width", 28)  
+      .attr("width", 41)  
       .attr("height", 0)
       .on('mouseover', tip.show)
       .on('mouseout', tip.hide)
@@ -256,15 +219,18 @@ var chart = d3.select("#detailed-overview-chart")
       .duration(1500)
       .attr("y", function (d) { return y(d.x);})
       .attr("height", function (d) { return (height - y(d.x)); })
-      .filter(function(d) { return d.x == 10; })
-      .style("fill", "#FC3E3E");
-    chart.selectAll("div")
+      .filter(function(d) { return d.element == arr[4].element; })
+      .style("fill", "#FC3E3E")
+      .attr("class", "fdasfds");
+  
+  chart.selectAll("div")
       .data(arr)
-    .enter().append("rect")
+      .enter().append("rect")
       .attr("class", "bar2")
-      .attr("x", function(d) {return g2(d.y) + 65; })
+      .style("fill", "#5C46A4")
+      .attr("x", function(d) {return g2(d.element) + 62; })
       .attr("y", height)
-      .attr("width", 28)
+      .attr("width", 41)
       .attr("height", 0)
       .on('mouseover', tip.show)
       .on('mouseout', tip.hide)
@@ -272,8 +238,12 @@ var chart = d3.select("#detailed-overview-chart")
       .duration(1500)
       .attr("y", function (d) { return y(d.y);})
       .attr("height", function (d) { return (height - y(d.y)); })
-            .filter(function(d) { return d.y == 327.9; })
-      .style("fill", "#3D2390");
+      .filter(function(d) { return d.element == arr[4].element; })
+        .style("fill", "#3D2390");
+
+  jQuery( "text:contains('2016-17')").css('fill', '#3D2390').parent().append(jQuery('<line>', {"class" : "Hello" ,"y1" : 26,"y2" : 26 ,"x1" : -48, "x2" : 47}));
+// jQuery(".Hello").text('activeyear');
+});
 
   (function ($){
     $(document).ready(function () {
