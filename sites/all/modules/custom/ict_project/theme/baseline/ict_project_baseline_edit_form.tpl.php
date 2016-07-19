e6ae2c5ab528f64a0d9e6dfb64538492a603ed75
Artem Bazykin <artem.bazykin@linkdigital.com.au>
2016-07-19 14:12:57 +0300

    TPDFINXCDDB-323 / Print preview btn changed implementation

--- sites/all/modules/custom/ict_project/theme/baseline/ict_project_baseline_edit_form.tpl.php
+++ sites/all/modules/custom/ict_project/theme/baseline/ict_project_baseline_edit_form.tpl.php
@@ -3,9 +3,7 @@
 	<div class="other-option-buttons">
 		<?php print render($form['edit']); ?>
 
-    <div class="pdf_button submit">
-      <span class="print_pdf"><a href="javascript:void(0);" title="Print this page." class="print-pdf export-btn" onclick="window.print(); return false" rel="nofollow">Print Preview</a></span>
-    </div>
+        <?php print theme('ict_project_print_pdf_button') ?>
 	</div>
     <?php print render($form['request_approve']); ?>
   </div>
