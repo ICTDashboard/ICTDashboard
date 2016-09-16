db6be1eb82e2fbcbcd5bad2baa88d29a0d69bda4
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2016-09-16 11:38:34 +1000

    SXTPDFINXZCF-16 / reset password updates

--- sites/all/themes/itdash/template.php
+++ sites/all/themes/itdash/template.php
@@ -287,12 +287,21 @@ function itdash_form_user_pass_reset_alter(&$form, &$form_state, $form_id) {
   $timestamp = $form_state['build_info']['args'][1];
   $timeout = variable_get('user_password_reset_timeout', 86400);
   $users = user_load_multiple(array($uid), array('status' => '1'));
-  $form['message'] = array('#markup' => t('<p>As a user of this application,<b> you agree to not enter data that has a security classification of PROTECTED or above.</b>
-                                            You also acknowledge the Disclaimer, Terms of Use and Privacy statements as set out in the footer of this website.</p>
-                                            <p><b>Agree</b> = continue registration/first time login</p>
-                                            <p><b>Not Agree</b> = Message “Your registration cannot continue unless you agree to these requirements” – stop process.</p>
-                                            <p>This is one-time login for %user_name and will expire on <span class="expiration-date-user-pass-reset">%expiration_date</span>.</p>
-                                            <p>If you agree, click on this button to log in to the site and change your password.</p>',
-                     array('%user_name' => $users[$uid]->name, '%expiration_date' => format_date($timestamp + $timeout, 'custom', 'D, d/m/Y - H:ia'))));
+  $agr_text = variable_get('agreement_text', array('value' =>'', 'format' => 'filtered_html'));
+  $form['message'] = array(
+    '#markup' => t('
+        <p>This is one-time login for %user_name and will expire on <span class="expiration-date-user-pass-reset">%expiration_date</span>.</p>
+        <p>If you agree to the terms of use, click on the <em>log in</em> button to change your password.</p>
+        <p><strong>Terms of use:</strong></p>
+        <div id="agreement-text-login">!arr_text</div>
+        <p>If you do not agree, click on the <em>home</em> button located at the top of the page to return to the Dashboard.</p>
+    ',
+    array(
+      '%user_name' => $users[$uid]->name,
+      '%expiration_date' => format_date($timestamp + $timeout, 'custom', 'D, d/m/Y - H:ia'),
+      '!arr_text' => $agr_text['value']
+      )
+    )
+  );
   $form['help'] = array('#markup' => '<p>' . t('This login can be used only once.') . '</p>');
 }
